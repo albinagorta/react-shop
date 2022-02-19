8adfff1860038f9401e09eef9ee35f24b08725fc
Angel Albinagorta Vargas <43548855+albinagorta@users.noreply.github.com>
2022-02-18 23:37:02 -0500

    cambios en mostrar cantidad de productos add a carrito en header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useContext} from 'react';
 import '@styles/Header.scss';
 
 import Menu from '@components/Menu';
@@ -7,13 +7,25 @@ import icon_menu from '@icons/icon_menu.svg';
 import icon_shopping_cart from '@icons/icon_shopping_cart.svg';
 import logo_yard_sale from '@logos/logo_yard_sale.svg';
 
+import AppContext from '@context/AppContext';
 
 const Header = () => {
-	const [toggle,setToggle]=useState(false);
+	const [ toggle, setToggle ] = useState(false);
+	const { state } = useContext(AppContext);
 
 	const handleToggle = () => {
 		setToggle(!toggle);
 	}
+
+	const verifyCart = (cartNumber) => {
+		if (cartNumber && (cartNumber < 10)) {
+		  return (<div>{cartNumber}</div>);
+		} else if (cartNumber > 9) {
+		  return (<div>+9</div>);
+		} else {
+		  return null;
+		}
+	  }
 
 	return (
 		<nav>
@@ -48,7 +60,7 @@ const Header = () => {
 					</li>
 					<li className="navbar-shopping-cart">
 						<img src={icon_shopping_cart} alt="shopping cart" />
-						<div>2</div>
+						{ verifyCart(state.cart.length) }
 					</li>
 				</ul>
 			</div>
