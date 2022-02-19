e2d0d328f5c9d53c9d82ea15cbe9aa471b8481a3
Angel Albinagorta Vargas <43548855+albinagorta@users.noreply.github.com>
2022-02-18 22:05:17 -0500

    creacion de hook usegetproducts con axios lista de prodcutos y listarlos

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,28 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
+//components
 import ProductItem from '@components/ProductItem';
-import '@styles/ProductList.scss';
 
+//hooks
+import useGetProducts from '@hooks/useGetProducts';
+
+//css
+import '@styles/ProductList.scss';
 
 const API = 'https://api.escuelajs.co/api/v1/products';
 
 
 const ProductList = () => {
 
-	const [products, setProducts] = useState([]);
-
-	useEffect(async () => {
-		const response = await axios(API);
-		setProducts(response.data);
-	}, [])
-
+	const products = useGetProducts(API);
 	return (
 		<section className="main-container">
 			<div className="ProductList">
 				{
 					products.map(product => (
-						<ProductItem />
+						<ProductItem product={product} key={product.id} />
 					))
 				}
 			</div>
