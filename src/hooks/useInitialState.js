85a5d8f236bae762723dfc3eff708f626d25437f
Angel Albinagorta Vargas <43548855+albinagorta@users.noreply.github.com>
2022-02-19 13:00:39 -0500

    add a cart lista y Eliminar producto de lista ordens

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -8,16 +8,27 @@ const useInitialState = () => {
     const [state, setState] = useState(initialState);
 
     const addToCart = (payload) => {
+        const payloadExist=state.cart.filter((item) => item.id == payload.id);
+        if(payloadExist.length==0){
+            setState({
+                ...state,
+               cart: [...state.cart, payload]
+            });
+        }
+    };
+
+    const removeFromCart = (payload) => {
         setState({
             ...state,
-            cart: [...state.cart, payload]
+            cart: state.cart.filter(
+                (item) => item.id != payload.id),
         });
-        console.log(state);
     };
 
     return {
         state,
-        addToCart
+        addToCart,
+        removeFromCart
     }
 
 }
